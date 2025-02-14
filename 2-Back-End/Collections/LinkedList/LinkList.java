1cc2fde67c178e6938813ae616f4354fe8b07010
RahulKarthick <rahulkarthickcce@gmail.com>
2025-02-14 11:54:58 +0530

    Modify LinkedList

--- 2-Back-End/Collections/LinkedList/LinkList.java
+++ 2-Back-End/Collections/LinkedList/LinkList.java
@@ -1,6 +1,5 @@
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.ListIterator;
 
 public class LinkList {
     public static void main(String[] args) {
@@ -9,13 +8,6 @@ public class LinkList {
         l.add("Two");
         l.add("Three");
         l.add("Four");
-        ListIterator i = l.listIterator(3);
-        while (i.hasNext()) {
-            i.next(); // used to make the cursor move to last value
-        }
-        while (i.hasPrevious()) {
-            System.out.println(" Reverse printing values using .next(): " + i.previous());
-        }
         Iterator li = l.descendingIterator(); // This method is used only with LinkedList
         while (li.hasNext()) {
             System.out.println(" Reverse printing using .descendingIterator(); " + li.next());
