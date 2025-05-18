29170909023a8180d0d83b64101ebbc88844d907
RahulKarthick <rahulkarthickcce@gmail.com>
2025-05-18 19:17:54 +0530

    Add Pattern

--- 2-Back-End/1-Core-Java/Pattern/DecreasingNumber.java
+++ 2-Back-End/1-Core-Java/Pattern/DecreasingNumber.java
@@ -1,8 +1,8 @@
-public class DecreasingCol {
+public class DecreasingNumber {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5 - i + 1; j++) {
-                System.out.print("* ");
+                System.out.print(j + " ");
             }
             System.out.println(" ");
         }
